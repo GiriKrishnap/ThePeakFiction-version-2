68edcc539ef092a88f2224bae48fbffa4fe4ce42
giri <girikrishna.p6@gmail.com>
2024-05-20 12:28:46 +0530

    added ?

--- Front-end/src/components/UserComponents/home/banner.jsx
+++ Front-end/src/components/UserComponents/home/banner.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { addNovelToLibraryAPI, getRandomNovelAPI } from '../../../APIs/userAPI';
-import {  novelDetailedView } from '../../../util/constants';
+import { novelDetailedView } from '../../../util/constants';
 import toast from 'react-hot-toast';
 //.........................................................................
 
@@ -78,19 +78,19 @@ export default function Banner() {
 
             <div className={`md:h-72 ${loading && "animate-pulse"} h-56 m-2 rounded-lg grid md:grid-cols-3 grid-cols-2 
             overflow-hidden`}
-                style={novel._id ? { backgroundImage: `url(${novel.cover})`, backgroundSize: 'cover' } : {}}>
+                style={novel?._id ? { backgroundImage: `url(${novel?.cover})`, backgroundSize: 'cover' } : {}}>
                 {/* //////////////// */}
                 <div className=' pl-5 flex flex-col place-items-start text-left bg-gradient-to-r
                  from-gray-800 from-50% to-transparent to-95% rounded-lg p-4'>
 
                     <p className={`${novel.title?.length < 15 ? "md:text-7xl" : 'md:text-6xl'} text-4xl m-1 
                      md:mt-8 mt-8 font-medium bold-text text-white drop-shadow-md hover:animate-pulse duration-1000 `}>
-                        {novel.title ? novel.title : 'loading... :)'}
+                        {novel?.title ? novel.title : 'loading... :)'}
                     </p>
 
                     <p className='text-gray-200 mt-3 italic md:h-36 h-full overflow-hidden 
                     poppins drop-shadow-lg hidden lg:block w-full'>
-                        {novel.description}
+                        {novel?.description}
                     </p>
 
                     {
@@ -113,7 +113,7 @@ export default function Banner() {
                 {/* //////////////// */}
 
                 {
-                    novel._id ?
+                    novel.length > 0 ?
                         <div
                             style={{ backgroundImage: `url(${novel.cover})`, backgroundSize: 'cover' }}
                             className='md:h-64 h-44 m-4 rounded-lg drop-shadow-2xl -rotate-6 bg-black md:w-44
@@ -124,8 +124,8 @@ export default function Banner() {
                 <div className='p-10 hidden lg:block mt-10 '>
 
                     <p className='flex text-red-400 poppins drop-shadow-lg font-bold text-2xl tracking-wider'>Author: {novel.author?.userName} </p>
-                    <p className='flex text-blue-100 poppins drop-shadow-md font-bold text-2xl tracking-wider'>Publish Date: {new Date(novel.publish_date).toLocaleDateString("en-GB")}</p>
-                    <p className='flex text-blue-100 poppins drop-shadow-md font-bold text-2xl tracking-wider'>Rating: {novel.rate}</p>
+                    <p className='flex text-blue-100 poppins drop-shadow-md font-bold text-2xl tracking-wider'>Publish Date: {new Date(novel?.publish_date).toLocaleDateString("en-GB")}</p>
+                    <p className='flex text-blue-100 poppins drop-shadow-md font-bold text-2xl tracking-wider'>Rating: {novel?.rate}</p>
 
                     <br />
 
