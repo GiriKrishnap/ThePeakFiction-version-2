f1fc071ea469bcd1762e0edcb1bb8d078ffc2288
giri <girikrishna.p6@gmail.com>
2024-05-28 11:34:46 +0530

    comment fix

--- Front-end/src/components/Comments/comments.jsx
+++ Front-end/src/components/Comments/comments.jsx
@@ -5,8 +5,8 @@ const Comments = ({ novelId, chapterNo = 0 }) => {
 
     const disqusShortname = 'thepeakfiction'; // Replace with your actual Disqus shortname
     const disqusConfig = {
-        url: `https://the-peak-fiction-version-2.vercel.app/${novelId + chapterNo}`, // Replace with your website URL
-        identifier: `#${novelId + chapterNo}`, // Replace with a unique identifier for each page
+        url: `https://the-peak-fiction-version-2.vercel.app`, // Replace with your website URL
+        identifier: `${novelId + chapterNo}`, // Replace with a unique identifier for each page
         title: `${novelId + chapterNo}`, // Replace with the title of your page
     };
 
